e6e38daf0bdba92819fb5bab8b540ca5395aa54a
bangshiuh <bangshiuh.chen@gmail.com>
2020-01-30 15:11:01 -0500

    enable burner-stabilized flame with mass-flux BC

--- src/flameSolver.cpp
+++ src/flameSolver.cpp
@@ -76,6 +76,7 @@ void FlameSolver::initialize(void)
     if (options.wallFlux) {
         diffusionTerms[kEnergy].yInf = options.Tinf;
         diffusionTerms[kEnergy].wallConst = options.Kwall;
+        rVzero = options.rVwall;
     }
 
     resizeAuxiliary();
